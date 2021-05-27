96f3c38cd5812886adee7160ff17c7fcfbccec2a
Jason Tan <tanjunxiang@guazi.com>
2021-05-28 00:23:08 +0800

    first commit

---
+++ src/views/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    <div class="index-title">这是一个Excel工具</div>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.index {
+  &-title {
+    font-size: 40px;
+    font-weight: bold;
+  }
+}
+</style>
